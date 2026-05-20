c34c7192f260f14bf42cbca216d383eafb6eaff9
kingsbalfx <shafiuabdullahi.sa3@gmail.com>
2026-05-20 22:38:34 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,26 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
         brand: {
-          50: '#eef7ff',
-          100: '#d8ebff',
-          200: '#b3d8ff',
-          300: '#80bfff',
-          400: '#4b9cff',
-          500: '#1f7bf7',
-          600: '#155cd6',
-          700: '#12469c',
-          800: '#103a7d',
-          900: '#0d3267'
-        }
-      }
-    }
+          50: "#eef7ff",
+          100: "#d8ebff",
+          200: "#b3d8ff",
+          300: "#80bfff",
+          400: "#4b9cff",
+          500: "#1f7bf7",
+          600: "#155cd6",
+          700: "#12469c",
+          800: "#103a7d",
+          900: "#0d3267",
+        },
+      },
+    },
   },
   plugins: [],
 }
